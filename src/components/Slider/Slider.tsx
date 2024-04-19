5fde4b883a3f2c0188a04e25bfb0af02f6a05bb5
Elisa ROGGE <elisa.rogge59@gmail.com>
2024-04-19 10:59:59 +0200

    fix: linear gradient, slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -20,17 +20,14 @@ const Slider: React.FC<SliderProps> = ({ pictures }) => {
 
   return (
     <div className="image-slider">
-      {currentImageIndex > 0 && (
-        <button className="slider-button prev" onClick={prevImage}>
-          &#10094;
-        </button>
-      )}
+      <button className="slider-button prev" onClick={prevImage}>
+        &#10094;
+      </button>
       <img src={pictures[currentImageIndex]} alt="" />
-      {currentImageIndex < pictures.length - 1 && (
-        <button className="slider-button next" onClick={nextImage}>
-          &#10095;
-        </button>
-      )}
+      <span className="numerotation">{currentImageIndex + 1}/{pictures.length}</span>
+      <button className="slider-button next" onClick={nextImage}>
+        &#10095;
+      </button>
     </div>
   );
 };
